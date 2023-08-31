72761a97a0202643585f3cf7be9236e5f31ebcb8
nidhalSadfi <nidhal.sadfi@gmail.com>
2023-08-31 10:23:54 +0100

    the day

---
+++ 31august/pointers_malloc/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    printf("Hello pointers !!!!\n");
+    printf("<<1>>. reserve static Stack addresses on int and leyt content variable!\n");
+    int i = 19, j = 90;
+    printf("i = %d [address of i = 0x%p]\n",i,&i);
+    printf("j = %d [address of j = 0x%p]\n",j,&j);
+    i = 45;
+    printf("i = %d [address of i = 0x%p]\n",i,&i);
+    printf("<<2>>. reserve static stack address on float and let content variable!\n");
+    float f = 19.7;
+    printf("f = %f [address of i = 0x%p]\n",f,&f);
+
+    int *pi ;// = &f;
+    printf("[1. address of pi = %p]\n",pi);
+    pi = malloc(sizeof(int));
+    *pi = 16;
+    printf("[2. address of pi = %p]\n",pi);
+    printf("now the content of *pi = %d\n",*pi);
+    free(pi);
+    printf("[3. address of pi = %p]\n",pi);
+    //*pi= 17 !!!!!!Plantage
+
+    int * px = malloc(sizeof(int));
+
+    pi = malloc(sizeof(int));
+    printf("[4. address of pi = %p]\n",pi);
+    *pi = 17;
+    printf("now the content of *pi = %d\n",*pi);
+    free(pi);
+
+    free(px);
+}
