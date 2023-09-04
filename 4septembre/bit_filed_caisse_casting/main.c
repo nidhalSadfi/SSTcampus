98a999f6c6cc859afc1559bccf2cd0644493b801
nidhalSadfi <nidhal.sadfi@gmail.com>
2023-09-04 11:38:58 +0100

    12am commit

---
+++ 4septembre/bit_filed_caisse_casting/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NONPAYE  0  //0000
+#define COMPTANT 1  //0001
+#define CHEQUE   2  //0010
+#define VIREMENT 4  //0100
+#define CREDIT   8  //1000
+
+typedef struct {
+    unsigned char nonpaye:1;
+    unsigned char comptant:1;
+    unsigned char cheque:1;
+    unsigned char virement:1;
+    unsigned char credit:1;
+    unsigned char m6:1;
+    unsigned char m7:1;
+    unsigned char m8:1;
+}toptions;
+
+int main() {
+    printf("sizeof(otions)=%d\n",sizeof(toptions));
+    unsigned char _opt = COMPTANT | CHEQUE |CREDIT;
+    printf("_opt = %d\n",_opt);
+    printf("1. %s comptant on stOpt\n",_opt&COMPTANT?"Par":"Sans");
+    printf("1. %s crédit on stOpt\n",_opt&CREDIT?"Par":"Sans");
+
+
+    toptions stOpt;
+    stOpt.comptant = 1;
+    stOpt.cheque = 1;
+    stOpt.virement = 1;
+    stOpt.credit = 0;
+    stOpt.nonpaye = 0;
+    stOpt.m6 =stOpt.m7 =stOpt.m8 = 0;
+
+    unsigned char *modepayment=(unsigned char*)&stOpt;
+    printf("_opt = %d\n",*modepayment); //save on database !!!the value 14 !
+///
+
+
+///
+//in order to retreive thje payemeny mode !!
+    unsigned char readOpt = *modepayment;
+    printf("_opt = %d\n",readOpt); //read from database !!!the value 14 !
+
+    toptions *pOpt = (toptions*)&stOpt;
+
+    printf("2. %s comptant on pOpt\n",pOpt->comptant?"Par":"Sans");
+    printf("2. %s cheque on pOpt\n",pOpt->cheque?"Par":"Sans");
+    printf("2. %s credit on pOpt\n",pOpt->credit?"Par":"Sans");
+
+    return 0;
+}
