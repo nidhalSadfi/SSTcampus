ac437553bc9aa6a188760d02b05f617514819508
nidhalSadfi <nidhal.sadfi@gmail.com>
2023-09-08 08:49:42 +0100

    fixes by M.O.

--- 6septembre/chained_list/main.c
+++ 6septembre/chained_list/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//by mayssa
+
 typedef unsigned int UINT; //delay the definition of the type at he end $$$
 
 //Chained list
@@ -9,6 +9,11 @@ struct lnode{
    int value;
    struct lnode *next;
 };
+
+
+
+
+
 int main()
 {
     printf("Hello world!\n");
@@ -95,26 +100,66 @@ last=lNewNode;
         }
         garde->id=garde->id+1;
 
-    printf("display the list..\n");
+        printf("display the list..\n");
     struct lnode *pWalker;
     pWalker =first;
+    int length =0;
     do{
       printf("[%d,%d]",pWalker->id, pWalker->value);
       if (pWalker->next == NULL)
         break;
       //now go to the next :
+      length++;
       pWalker = pWalker->next;
     }while(1);
     printf("\n");
+    printf("\n");
+    printf("la longueur de la liste chainee est %d",(length+1));
+    printf("\n");
 
 
+printf("Suppression de la liste sauf tete !\n");
+    struct tnode *pSave;
+
+    if (first -> next == NULL)
+        printf("il n'existe que la tete dans la liste impossible de supprimer");
 
 
+    else{
+        pWalker = first-> next;
+        int nb = 0;
+        do{
+              if (pWalker == NULL)
+                break;
+              pSave = pWalker->next;
+              free(pWalker);
+              nb++;
+              pWalker = pSave;
+        }while(1);
+        printf("liberate %d element on the list \n",nb);
+        printf("\n");
+        printf("\n");
+        printf("recalcuting the length");
+        int lengthSupp =0;
+        struct lnode *pWalker2;
+        pWalker2 =first;
 
+        do{
+            if (pWalker2->next == NULL)
+               lengthSupp++;
+            break;
+            pWalker = pWalker->next;
+        }while(1);
+        printf("\n");
+        printf("\n");
+        printf("la longueur de la liste chainee apres suppression est %d",(lengthSupp+1));
+        printf("\n");
+    }
+
+/*
     printf("libererate the memory !\n");
     struct tnode *pSave;
     pWalker = first;
-
     int nb = 0;
     do{
       if (pWalker == NULL)
@@ -125,5 +170,7 @@ last=lNewNode;
       pWalker = pSave;
     }while(1);
     printf("liberate %d element on the list \n",nb);
+    */
     return 0;
 }
+//full work}
