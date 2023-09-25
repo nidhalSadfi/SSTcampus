6247ed997db48079b0f9fc4bf6cd3e6d388c1d52
nidhalSadfi <nidhal.sadfi@gmail.com>
2023-09-25 16:38:26 +0100

    25/09

--- 25septembre/path/main.c
+++ 25septembre/path/main.c
@@ -1,10 +1,29 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-int main(){
+#define TERMINASION ".log"
+int main(int arc , char *argv[]){
     char path[250];
-    
+    char *pMyStr;
+    printf("hello executable name: %s \n",argv[0]);
+    char *lastdot=strrchr(argv[0],'.');
+    int len;
+    if(lastdot){
+        len=(lastdot-argv[0]);
+        pMyStr=malloc(len+strlen(TERMINASION)+1);
+        strncpy(pMyStr,argv[0],len);
+        pMyStr[len]='\0';
+        strcat(pMyStr,TERMINASION);
+    }
+    else{
+        pMyStr=malloc(strlen(argv[0])+strlen(TERMINASION)+1);
+        strcpy(pMyStr,argv[0]);
+        strcat(pMyStr,TERMINASION);
+    }
+    printf("new name %s \n",pMyStr);
+    free(pMyStr);
     do{
+        
         printf("give me your file path sir\n");
         scanf("%s",path);
         if(!strcmp(path,"0"))
