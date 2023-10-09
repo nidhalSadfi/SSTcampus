de5171a342e33e1e175a9409d39960ed85e5212c
nidhalSadfi <nidhal.sadfi@gmail.com>
2023-10-09 14:48:00 +0100

    make files and all

---
+++ 9october/compiler/CampusLex/lexparserExt.h
@@ -0,0 +1,9 @@
+// analex.cpp : Defines the entry point for the application.
+//
+#include <inc/automate.h>
+
+//fonctions exportées
+void initLexParser(char *sNomFich);
+extern void startLexParser();
+extern void dumpLexParser(char *sNomFich);
+extern void libererLexParser();
